a03f26bb9d7ef29301a7fa9c05fad31bb78c82f5
02714395058 <02714395058@ine>
2019-09-04 21:50:40 -0300

    aula 04/09 fix

--- Lista Duplamente Encadeada/src/lista/duplamente/encadeada/ListaDuplamenteEncadeada.java
+++ Lista Duplamente Encadeada/src/lista/duplamente/encadeada/ListaDuplamenteEncadeada.java
@@ -35,5 +35,7 @@ public class ListaDuplamenteEncadeada {
      * acessaAtual 
     */
     
+    private NodoDuplo inicio, fim;
+    
 }
 
---
+++ Lista Duplamente Encadeada/src/lista/duplamente/encadeada/NodoDuplo.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista.duplamente.encadeada;
+
+/**
+ *
+ * @author 02714395058
+ */
+public class NodoDuplo {
+    
+    private NodoDuplo proximo, anterior;
+    private Object elemento;
+
+    public NodoDuplo(NodoDuplo proximo, NodoDuplo anterior, Object elemento) {
+        this.proximo = proximo;
+        this.anterior = anterior;
+        this.elemento = elemento;
+    }
+
+    public NodoDuplo getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NodoDuplo proximo) {
+        this.proximo = proximo;
+    }
+
+    public NodoDuplo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoDuplo anterior) {
+        this.anterior = anterior;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+    
+        
+}
